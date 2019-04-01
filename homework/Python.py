2a729982636186d7aa6ccea99d383ea2dd5b076d
Bonny Huang <genioushoney14@gmail.com>
2019-04-01 14:51:32 +0800

    Create Python.py

---
+++ homework/Python.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 31 18:13:28 2019
+
+@author: Bonny Huang
+"""
+import pandas as banana
+
+df=banana.DataFrame({'a':[11,21,31],'b':[21,22,23]})
+
+df.head()
